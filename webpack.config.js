4735448d47f471547502863f821ebcae9ec18cee
Tristan Wright <tristan.wright@kitware.com>
2015-07-21 12:33:44 -0600

    added sample-data.json, changes to viewer.js for Arctic viewer, jshint warning options

--- webpack.config.js
+++ webpack.config.js
@@ -20,13 +20,15 @@ module.exports = {
       }
     ],
     loaders: [
-      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=60000&minetype=application/font-woff" },
+      { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=60000&mimetype=application/font-woff" },
       { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=60000" },
       { test: /\.css$/, loader: "style-loader!css-loader!autoprefixer-loader?browsers=last 2 version" }
     ]
   },
   jshint: {
     esnext: true,
+    devel: true, // suppress alert and console global warnings
+    browser: true, // suppress global browser object warnings
     globalstrict: true // Babel add 'use strict'
   }
 };
