b40ea44e7404d486dafe3224f5fff5253120f621
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2015-10-01 18:01:16 -0600

    chore(build): Add bundle script
    npm run build:bundle

---
+++ bin/bundle.js
@@ -0,0 +1,76 @@
+#! /usr/bin/env node
+
+require('shelljs/global');
+
+var path = require('path'),
+    tarball = require('tarball-extract'),
+    nodeDest = path.join(__dirname, '../bundles/nodes'),
+    bundleRoot = path.join(__dirname, '../bundles'),
+    bundleIdx = 0,
+    directoryToCopy = [
+        path.join(__dirname, '../bin'),
+        path.join(__dirname, '../dist'),
+        path.join(__dirname, '../node_modules'),
+    ],
+    nodePaths = [
+        { name: 'osx', url: 'https://nodejs.org/dist/v4.1.1/node-v4.1.1-darwin-x64.tar.gz' },
+        { name: 'linux_x64', url: 'https://nodejs.org/dist/v4.1.1/node-v4.1.1-linux-x64.tar.gz' },
+        { name: 'linux_x86', url: 'https://nodejs.org/dist/v4.1.1/node-v4.1.1-linux-x86.tar.gz' },
+        // { name: 'win32', url: [ 'https://nodejs.org/dist/v4.1.1/win-x86/node.exe', 'https://nodejs.org/dist/v4.1.1/win-x86/node.lib'] },
+        // { name: 'win64', url: [ 'https://nodejs.org/dist/v4.1.1/win-x64/node.exe', 'https://nodejs.org/dist/v4.1.1/win-x64/node.lib'] }
+    ];
+
+// Create download destination
+rm('-rf', bundleRoot);
+mkdir('-p', nodeDest);
+
+// Process next bundle
+function next() {
+    if(bundleIdx < nodePaths.length) {
+        var bundle = nodePaths[bundleIdx++];
+        generateBundle(bundle);
+    } else {
+        // Clean download
+        rm('-rf', nodeDest);
+    }
+}
+
+// Generate a bundle
+function generateBundle(nodeConf) {
+    var filePath = nodeConf.url.split('/').pop(),
+        bundleDest = path.join(bundleRoot, nodeConf.name);
+        nodeExecDest = path.join(bundleDest, 'bin');
+
+    // Copy ArcticViewer into bundle
+    directoryToCopy.forEach(function(d) {
+        cp('-r', d, bundleDest);
+    })
+
+    // Create Exec file
+    var execScript = path.join(bundleDest, 'ArcticViewer.sh');
+    './bin/node ./bin/arctic-viewer-cli.js $@'.to(execScript);
+    chmod('u+x', execScript);
+
+    // Download node and put the binary into bundle
+    tarball.extractTarballDownload(
+        nodeConf.url,
+        path.join(nodeDest, filePath),
+        nodeDest,
+        {},
+        function(err, result) {
+            if(!err) {
+                // copy node exec
+                var dirName = filePath.split('.tar')[0];
+                cp(path.join(nodeDest, dirName, 'bin', 'node'), nodeExecDest);
+
+                // Move to next bundle
+                next();
+            } else {
+                console.log(' | oups something wrong happened while downloading ' + url);
+            }
+        }
+    );
+}
+
+// Start bundle process
+next();
