5fad92b15454c8f1c7ab4b69f90d8db8788104dd
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2015-12-18 14:22:12 -0700

    fix(Babel6): Fix import/export for Babel 6

--- webpack.config.js
+++ webpack.config.js
@@ -10,22 +10,20 @@ module.exports = {
   module: {
     preLoaders: [
       {
-          test: /\.js$/,
-          exclude: /node_modules/,
-          loader: "jshint-loader!babel" // ?optional[]=runtime&optimisation.react.inlineElements
-      },{
-          test: /\.js$/,
-          include: /tonic-/,
-          loader: "babel" // ?optional[]=runtime&optimisation.react.inlineElements
+            test: /\.js$/,
+            loader: "eslint-loader",
+            exclude: /node_modules/
       }
     ],
     loaders: [
+      { test: require.resolve("./lib/arctic-viewer.js"), loader: "expose?ArcticViewer" },
       { test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=60000&mimetype=application/font-woff" },
       { test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/, loader: "url-loader?limit=60000" },
       { test: /\.(png|jpg)$/, loader: 'url-loader?limit=8192'},
       { test: /\.css$/, loader: "style-loader!css-loader!autoprefixer-loader?browsers=last 2 version" },
       { test: /\.c$/i, loader: "shader" },
-      { test: require.resolve("./lib/arctic-viewer.js"), loader: "expose?ArcticViewer" }
+      { test: /\.js$/, include: /node_modules\/tonic-/, loader: "babel?presets[]=react,presets[]=es2015" },
+      { test: /\.js$/, exclude: /node_modules/, loader: "babel?presets[]=react,presets[]=es2015" }
     ]
   },
   jshint: {
@@ -36,5 +34,8 @@ module.exports = {
   },
   externals: {
     "three": "THREE"
+  },
+  eslint: {
+      configFile: '.eslintrc'
   }
 };
