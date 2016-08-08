00d121c879305684529859456cd928c0368cc66c
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2016-08-08 15:18:19 -0600

    fix(ParaViewWeb): Update dependency with build tools

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,14 @@
 module.exports = {
   extends: 'airbnb',
   rules: {
-    'max-len': [1, 160, 4, {"ignoreUrls": true}],
-    // 'no-console': 0,
-    'no-multi-spaces': [2, { exceptions: { "ImportDeclaration": true } }],
-    // 'no-nested-ternary': 0,
-    'no-param-reassign': [2, { props: false }],
-    'no-unused-vars': [2, { args: 'none' }],
+    'import/no-extraneous-dependencies': ["error", { "devDependencies": true }],
+    'max-len': ["warn", 160, 4, {"ignoreUrls": true}],
+    'no-multi-spaces': ["error", { exceptions: { "ImportDeclaration": true } }],
+    'no-param-reassign': ["error", { props: false }],
+    'no-unused-vars': ["error", { args: 'none' }],
+    'react/jsx-filename-extension': ["error", { "extensions": [".js"] }],
+
     'no-var': 0,
-    // 'one-var': 0,
-    // 'react/no-is-mounted': 1,
     'react/prefer-es6-class': 0,
   }
 };
