87a5c891e834b2387109e498dc634cb65d52f7e3
Sebastien Jourdain <sebastien.jourdain@kitware.com>
2018-11-06 11:48:17 -0700

    fix(rules): Update rules to properly import vtk.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,5 @@
 const path = require('path');
-
-const rules = require('paraviewweb/config/webpack.loaders.js');
+const autoprefixer = require('autoprefixer');
 
 const entry = path.join(__dirname, './lib/arctic-viewer.js');
 const outputPath = path.join(__dirname, './dist');
@@ -17,16 +16,132 @@ module.exports = {
     libraryTarget: 'umd',
   },
   module: {
-    rules: [{ test: entry, loader: 'expose-loader?ArcticViewer' }].concat(
-      rules,
+    rules: [
+      { test: entry, loader: 'expose-loader?ArcticViewer' },
+      {
+        test: /\.worker\.js$/,
+        include: /vtk\.js/,
+        use: [
+          {
+            loader: 'worker-loader',
+            options: { inline: true, fallback: false },
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        exclude: /\.module\.css$/,
+        use: [
+          'style-loader',
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: () => [autoprefixer('last 2 version', 'ie >= 10')],
+            },
+          },
+        ],
+      },
+      {
+        test: /\.css$/,
+        include: /\.module\.css$/,
+        use: [
+          { loader: 'style-loader' },
+          {
+            loader: 'css-loader',
+            options: {
+              localIdentName: '[name]-[local]_[sha512:hash:base64:5]',
+              modules: true,
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: () => [autoprefixer('last 2 version', 'ie >= 10')],
+            },
+          },
+        ],
+      },
+      {
+        test: /\.mcss$/,
+        use: [
+          { loader: 'style-loader' },
+          {
+            loader: 'css-loader',
+            options: {
+              localIdentName: '[name]-[local]_[sha512:hash:base64:5]',
+              modules: true,
+            },
+          },
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: () => [autoprefixer('last 2 version', 'ie >= 10')],
+            },
+          },
+        ],
+      },
+      {
+        test: /\.svg$/,
+        loader: 'svg-sprite-loader?runtimeCompat=true',
+        exclude: /fonts/,
+      },
+      {
+        test: /\.woff(2)?(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url-loader?limit=60000&mimetype=application/font-woff',
+      },
+      {
+        test: /\.(ttf|eot|svg)(\?v=[0-9]\.[0-9]\.[0-9])?$/,
+        loader: 'url-loader?limit=60000',
+        include: /fonts/,
+      },
+      {
+        test: /\.(png|jpg)$/,
+        loader: 'url-loader?limit=8192',
+      },
+      {
+        test: /\.c$/i,
+        loader: 'shader-loader',
+      },
+      {
+        test: /\.html$/,
+        loader: 'html-loader',
+      },
+      {
+        test: /\.isvg$/,
+        loader: 'html-loader?attrs=false',
+      },
       {
         test: /\.js$/,
-        loader: 'eslint-loader',
+        include: /paraviewweb/,
+        loader: 'babel-loader?presets[]=env,presets[]=react,babelrc=false',
+      },
+      {
+        test: /\.js$/,
+        include: /vtk.js/,
+        loader: 'babel-loader?presets[]=env,presets[]=react,babelrc=false',
+      },
+      {
+        test: /\.js$/,
+        include: /wslink/,
+        loader: 'babel-loader?presets[]=env,babelrc=false',
+      },
+      {
+        test: /\.glsl$/,
+        loader: 'shader-loader',
+      },
+      {
+        test: /\.js$/,
         exclude: /node_modules/,
-        enforce: 'pre',
-        options: { configFile: eslintrcPath },
-      }
-    ),
+        loader: 'babel-loader?presets[]=env,presets[]=react',
+      },
+    ].concat({
+      test: /\.js$/,
+      loader: 'eslint-loader',
+      exclude: /node_modules/,
+      enforce: 'pre',
+      options: { configFile: eslintrcPath },
+    }),
   },
   externals: {
     three: 'THREE',
